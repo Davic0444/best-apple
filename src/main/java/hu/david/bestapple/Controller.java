a2f9a256fa25d35f3dfdd8ee5e35c31c9959d132
Davic0444 <34513322+Davic0444@users.noreply.github.com>
2022-03-05 00:18:08 +0100

    Complete task

--- src/main/java/hu/david/bestapple/Controller.java
+++ src/main/java/hu/david/bestapple/Controller.java
@@ -1,9 +1,10 @@
 package hu.david.bestapple;
 
+import hu.david.dao.OrderDao;
 import hu.david.dao.ProductDao;
+import hu.david.model.Order;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -14,17 +15,86 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
-    private ComboBox<String> products;
+    private TextField tfCustomerName;
+    @FXML
+    private TextField tfEmail;
+    @FXML
+    private TextField tfPhoneNumber;
+    @FXML
+    private TextField tfPostCode;
+    @FXML
+    private TextField tfCity;
+    @FXML
+    private TextField tfAddress;
+    @FXML
+    private TextField tfAmount;
+    @FXML
+    private ComboBox<String> cbproducts;
+    @FXML
+    private CheckBox chLoweredPrice;
+    @FXML
+    private Button btnOrderButton;
+
 
     @FXML
-    public void selectProduct(){
+    void selectProduct(){
+        ProductDao productDao = new ProductDao();
+        ObservableList<String> list = FXCollections.observableArrayList(productDao.getAllActiveProductNamesWithPrice());
+        cbproducts.setItems(list);
+    }
+
+    @FXML
+    void placeOrder() {
+        String name = tfCustomerName.getText();
+        String email = tfEmail.getText();
+        String phone = tfPhoneNumber.getText();
+        String postalCode = tfPostCode.getText();
+        String city = tfCity.getText();
+        String address = tfAddress.getText();
+        Integer amount = Integer.parseInt(tfAmount.getText());
+        Integer price = Integer.parseInt(cbproducts.getSelectionModel().getSelectedItem().split("-")[1]);
+        String product = cbproducts.getSelectionModel().getSelectedItem().split("-")[0];
+        Boolean reduced = chLoweredPrice.isSelected();
+        Double totalSum = reduced ? amount * price * 0.8 : amount * price;
+
+        if (name.isEmpty()
+                || email.isEmpty()
+                || phone.isEmpty()
+                || postalCode.isEmpty()
+                || city.isEmpty()
+                || address.isEmpty()
+        ) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("error1");
+            alert.setHeaderText("error2");
+            alert.setContentText("error3");
+            alert.showAndWait();
+
+        } else {
+
+            Order obj = new Order(
+                    name,
+                    email,
+                    phone,
+                    postalCode,
+                    city,
+                    address,
+                    amount,
+                    product,
+                    reduced,
+                    totalSum);
+            OrderDao orderDaoObj = new OrderDao();
+            orderDaoObj.saveOrder(obj);
+            Alert orderAlert = new Alert(Alert.AlertType.INFORMATION);
+            orderAlert.setTitle("Sikeres megrendel�s");
+            orderAlert.setContentText("K�sz�nj�k a megrendel�st, hamarosan jelentkez�nk!!!");
+            orderAlert.showAndWait();
+
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ProductDao productDao = new ProductDao();
-        ObservableList<String> list = FXCollections.observableArrayList(productDao.getAllActiveProductNamesWithPrice());
-        products.setItems(list);
-        products.getSelectionModel().select(0);
+        selectProduct();
     }
 }
